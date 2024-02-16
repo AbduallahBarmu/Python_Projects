92fac1a2291dea791d08831f5383c1226d1b7bda
abduallahbarmu <abduallah.imu2@gmail.com>
2024-02-16 16:08:51 +0300

    update guess game into v1.1

--- number_guesser.py
+++ number_guesser.py
@@ -1,35 +1,43 @@
 import random
 
-top_of_range = input("Type a number: ")
+def get_top_of_range():
+    while True:
+        top_of_range = input("Type a number: ")
+        if top_of_range.isdigit():
+            top_of_range = int(top_of_range)
+            if top_of_range <= 0:
+                print('Please type a number larger than 0 next time.')
+            else:
+                return top_of_range
+        else:
+            print('Please type a number next time.')
+            
+            
+def play_game():
+    top_of_range = get_top_of_range()
+    random_number = random.randint(0, top_of_range)
+    guesses = 0
 
-if top_of_range.isdigit():
-    top_of_range = int(top_of_range)
+    while True:
+        guesses += 1
+        user_guess = input("Make a guess: ")
+        if user_guess.isdigit():
+            user_guess = int(user_guess)
+        else:
+            print('Please type a number next time.')
+            continue
 
-    if top_of_range <= 0:
-        print('Please type a number larger than 0 next time.')
-        quit()
-else:
-    print('Please type a number next time.')
-    quit()
+        if user_guess == random_number:
+            print("You got it!")
+            break
+        elif user_guess > random_number:
+            print("You were above the number!")
+        else:
+            print("You were below the number!")
 
-random_number = random.randint(0, top_of_range)
-guesses = 0
+    print("Congrats :) You got it in", guesses, "guesses")
 
-while True:
-    guesses += 1
-    user_guess = input("Make a guess: ")
-    if user_guess.isdigit():
-        user_guess = int(user_guess)
-    else:
-        print('Please type a number next time.')
-        continue
 
-    if user_guess == random_number:
-        print("You got it!")
-        break
-    elif user_guess > random_number:
-        print("You were above the number!")
-    else:
-        print("You were below the number!")
 
-print("You got it in", guesses, "guesses")
+if __name__ == "__main__":
+    play_game()
